500d8d57ed7b10c52b1c508004285b5a25a4970a
johanna-insightnl <johanna@insight.nl>
2024-07-01 14:14:12 +0200

    create logic to filter the color swatch with text input

--- src/UI/inputTextField/InputTextField.tsx
+++ src/UI/inputTextField/InputTextField.tsx
@@ -1,13 +1,18 @@
 import { Form } from 'react-bootstrap'
 
 interface TextInputProps {
+  value: string;
   placeholder?: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function InputTextField({ placeholder }: TextInputProps) {
+export default function InputTextField({ placeholder, value, onChange }: TextInputProps) {
   return (
     <Form.Control
-      placeholder={placeholder}
+    type="text"
+    placeholder={placeholder}
+    value={value}
+    onChange={onChange}
   />
   )
 }
--- src/components/ColorList/ColorList.tsx
+++ src/components/ColorList/ColorList.tsx
@@ -1,14 +1,30 @@
 import ColorListItem from "./ColorListItem";
 import colors from "./colors.json";
 import InputTextField from "../../UI/inputTextField/InputTextField";
+import { useState } from "react";
 
 export default function ColorList() {
+  const [searchText, setSearchText] = useState("");
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(event.target.value);
+  };
+
+  const sortedColors = colors.sort((a, b) => a.name.localeCompare(b.name));
+
+  const filteredColors = sortedColors.filter(color =>
+    color.name.toLowerCase().includes(searchText.toLowerCase())
+  );
+
   return (
     <>
-      <h1>Color List</h1>
-      <InputTextField placeholder="Search for a color swatch" />
+      <InputTextField
+        placeholder="Search for a color swatch"
+        value={searchText}
+        onChange={handleInputChange}
+      />
       {
-        colors.map((color) => (
+        filteredColors.map((color) => (
           <ColorListItem key={color.id} color={color} />
         ))
       }
