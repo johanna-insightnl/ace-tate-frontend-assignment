cdd9e63aeaa42640d20c805efa825404d68b6fe5
johanna-insightnl <johanna@insight.nl>
2024-07-01 13:17:01 +0200

    create acetate and duotone UI component

---
+++ src/UI/ColorCircle/acetate/AcetateColorCircle.tsx
@@ -0,0 +1,9 @@
+interface Props {
+  primary: string;
+}
+
+export default function AcetateColorCircle({ primary }: Props){
+  return(
+    <div className="acetate-circle-color"/>
+  )
+}
---
+++ src/UI/ColorCircle/duotone/DuotoneColorCircle.tsx
@@ -0,0 +1,9 @@
+interface Props {
+  primary: string;
+}
+
+export default function DuotoneColorCircle({ primary }: Props){
+  return(
+    <div className="duotone-circle-color"/>
+  )
+}
