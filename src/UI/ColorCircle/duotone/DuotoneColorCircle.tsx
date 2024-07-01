00430df6b71fefbf2b47d94c5e5a3f70259fbc98
johanna-insightnl <johanna@insight.nl>
2024-07-01 13:43:09 +0200

    create circle with duo tone and circle with acetate duo colors

--- src/UI/ColorCircle/duotone/DuotoneColorCircle.tsx
+++ src/UI/ColorCircle/duotone/DuotoneColorCircle.tsx
@@ -1,9 +1,15 @@
 interface Props {
   primary: string;
+  secondary?: string;
 }
 
-export default function DuotoneColorCircle({ primary }: Props){
+export default function DuotoneColorCircle({ primary, secondary }: Props){
+
+  const circleStyle = {
+    background: `linear-gradient(45deg, ${primary} 0%, ${primary} 50%, ${secondary} 50%, ${secondary} 100%)`,
+  };
+
   return(
-    <div className="duotone-circle-color"/>
+    <div className="single-circle-color" style={circleStyle}  />
   )
 }
--- src/components/ColorList/ColorListItem.tsx
+++ src/components/ColorList/ColorListItem.tsx
@@ -1,4 +1,6 @@
 import SingleColorCircle from "../../UI/ColorCircle/single/SingleColorCircle";
+import AcetateColorCircle from "../../UI/ColorCircle/acetate/AcetateColorCircle";
+import DuotoneColorCircle from "../../UI/ColorCircle/duotone/DuotoneColorCircle";
 
 interface Color {
   id: string;
@@ -14,12 +16,27 @@ interface Color {
 interface Props {
   color: Color;
 }
+
 export default function ColorListItem({ color }: Props){
   const { name, color: { primary, secondary }, pattern } = color;
 
+  let circleColorComponent;
+
+  switch(pattern){
+    case "acetate":
+      circleColorComponent = <AcetateColorCircle primary={primary} secondary={secondary}/>
+      break;
+    case "duotone":
+      circleColorComponent = <DuotoneColorCircle primary={primary} secondary={secondary}/>
+      break;
+    default:
+      circleColorComponent = <SingleColorCircle primary={primary}/>
+      break;
+  }
+
   return (
     <div className="d-flex align-items-center">
-      <SingleColorCircle primary={primary}/>
+      {circleColorComponent}
       <p>{name}</p>
     </div>
   )
