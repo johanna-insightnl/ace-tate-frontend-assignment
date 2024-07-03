2875f2ad94bac4665bb83438950b2a12bc7fa8c5
johanna-insightnl <johanna@insight.nl>
2024-07-03 13:29:57 +0200

    clean app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import './styles/main.scss'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { Col, Row, Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { ColorProvider } from './contexts/ColorContext';
 import ColorList from './components/ColorList/ColorList';
 import ColorForm from './components/ColorForm/ColorForm';
-import { ColorProvider } from './contexts/ColorContext';
+import './styles/main.scss'
 
 function App() {
   return (
